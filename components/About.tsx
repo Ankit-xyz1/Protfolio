d80a0afd9a4b64f50ad5d89538dbea7158360204
Ankit-xyz1 <shahankit023@gmail.com>
2025-04-23 21:30:32 +0530

    about section builded

--- components/About.tsx
+++ components/About.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import React, { useState } from 'react'
+import { SquarePenIcon } from './ui/square-pen'
+import { AtSignIcon } from './ui/at-sign'
 
 const About = () => {
+
   return (
-    <div>About</div>
+
+    <motion.div
+      initial={{ opacity: 0, x: -80 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}>
+      <div className='intro text-sm md:text-xl p-3'>
+        <span className='text-yellow-50'>Hi, <span className='font-semibold'>I'm Ankit S Gupta</span>, a passionate Full Stack Developer from Mumbai,</span><span className='text-zinc-400'> currently pursuing my studies at Nagindas Khandwala College (NKC). At just 20 years old, I've built a strong foundation in web development and consistently maintained a 9.0 CGPA throughout my academics.</span>
+        <span className='text-zinc-400'>
+          I specialize in building scalable and responsive web applications using the MERN stack (MongoDB, Express.js, React, Node.js)</span>
+      </div>
+      <div className='w-full h-fit flex flex-col justify-start text-white'>
+        <div className='w-fit flex gap-0 items-center '><AtSignIcon /> 0xanki7@gmail.com</div>
+        <div className='w-fit flex gap-0 items-center'><SquarePenIcon />My Blog <span className='text-xs ml-2 text-zinc-500'>not yet posted</span></div>
+      </div>
+    </motion.div>
   )
 }
 
---
+++ components/AbouteMe.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AbouteMe = () => {
+  return (
+    <div className='text-yellow-50'></div>
+  )
+}
+
+export default AbouteMe
